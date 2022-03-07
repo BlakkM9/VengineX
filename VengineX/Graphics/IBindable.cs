6be3cb4cf0ca1d02cfe87ba204e6219304048d6d
BlakkM9 <julian.desloges@gmail.com>
2022-03-07 17:49:49 +0100

    Started implementing graphic classes

---
+++ VengineX/Graphics/IBindable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VengineX.Graphics
+{
+    /// <summary>
+    /// Interface for bindable OpenGL resources.
+    /// </summary>
+    public interface IBindable
+    {
+        /// <summary>
+        /// Binds the resource to the current render state.
+        /// </summary>
+        void Bind();
+
+
+        /// <summary>
+        /// Unbinds the resource from the current render state.
+        /// </summary>
+        void Unbind();
+    }
+}
---
+++ VengineX/Graphics/Rendering/Shaders/Shader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VengineX.Graphics.Rendering.Shaders
+{
+    public class Shader : IBindable, IDisposable
+    {
+
+
+        public void Bind()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Unbind()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #region IDisposable
+
+        private bool _disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~Shader()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
