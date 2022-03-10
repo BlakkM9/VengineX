2d2419f0b1f8714fdc4e6b75adc8b88fe5564c40
BlakkM9 <julian.desloges@gmail.com>
2022-03-10 14:25:11 +0100

    Renamed Pipeline to Pipelines

--- VengineX/Graphics/Rendering/Pipelines/IRenderPipeline.cs
+++ VengineX/Graphics/Rendering/Pipelines/IRenderPipeline.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VengineX.Graphics.Rendering.Pipeline
+namespace VengineX.Graphics.Rendering.Pipelines
 {
     /// <summary>
     /// Interface for render pipelines.
--- VengineX/Graphics/Rendering/Pipelines/RenderPipelineBase.cs
+++ VengineX/Graphics/Rendering/Pipelines/RenderPipelineBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VengineX.Graphics.Rendering.Pipeline
+namespace VengineX.Graphics.Rendering.Pipelines
 {
     /// <summary>
     /// Base class for rendering pipelines.
