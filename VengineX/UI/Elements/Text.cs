bd44812635f33d7039e4053954dd8899dabf6f43
BlakkM9 <julian.desloges@gmail.com>
2022-03-14 07:51:09 +0100

    Implemented basic bitmap font loading/creation

--- VengineX/UI/Elements/Text.cs
+++ VengineX/UI/Elements/Text.cs
@@ -1,9 +1,14 @@
-﻿using OpenTK.Mathematics;
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VengineX.Graphics.Rendering;
+using VengineX.Graphics.Rendering.Shaders;
+using VengineX.Graphics.Rendering.Vertices;
+using VengineX.Resources;
 using VengineX.UI.Fonts;
 using VengineX.Wrappers.FreeType;
 
@@ -14,15 +19,70 @@ namespace VengineX.UI.Elements
     /// </summary>
     public class Text : UIElement
     {
-        public Text(BitmapFont font, string text, float x, float y, float size, Vector3 color)
+        public static Shader BitmapFontShader { get; private set; } = null;
+        public static int ProjectionMatrixLocation { get; private set; }
+        public static int ModelMatrixLocation { get; private set; }
+        public static int ViewMatrixLocation { get; private set; }
+        public static int ColorLocation { get; private set; }
+
+        public Vector4 Color { get => _color; set => _color = value; }
+        protected Vector4 _color;
+
+        private Mesh<UIVertex> _textMesh;
+        private BitmapFont _font;
+
+
+        public Text(BitmapFont font, string text, float x, float y, float size, Vector4 color)
             : base(x, y, font.CalculateWidth(text), size)
         {
+            // Lazy shader initialization
+            if (BitmapFontShader == null)
+            {
+                BitmapFontShader = ResourceManager.GetResource<Shader>("shader.bmpfont");
+                ProjectionMatrixLocation = BitmapFontShader.GetUniformLocation("P");
+                ModelMatrixLocation = BitmapFontShader.GetUniformLocation("M");
+                ViewMatrixLocation = BitmapFontShader.GetUniformLocation("V");
+            }
 
+            _font = font;
+            _textMesh = font.CreateMesh(text);
+            _color = color;
+
+            CalculateModelMatrix();
         }
 
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public override void Render()
         {
-            throw new NotImplementedException();
+            BitmapFontShader.Bind();
+            _font.TextureAtlas.Bind();
+
+            BitmapFontShader.SetUniformMat4(ProjectionMatrixLocation, ref ParentCanvas.ProjectionMatrix);
+            BitmapFontShader.SetUniformMat4(ViewMatrixLocation, ref ParentCanvas.ViewMatrix);
+            BitmapFontShader.SetUniformMat4(ModelMatrixLocation, ref ModelMatrix);
+            BitmapFontShader.SetUniformVec4(ColorLocation, ref _color);
+
+            //GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Line);
+            _textMesh.Render();
+            //GL.PolygonMode(MaterialFace.FrontAndBack, PolygonMode.Fill);
+        }
+
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        protected override void CalculateModelMatrix()
+        {
+            if (_font != null)
+            {
+                // Update model matrix
+                ModelMatrix = Matrix4.Identity;
+                ModelMatrix *= Matrix4.CreateScale(Height / _font.Size, Height / _font.Size, 0);
+                ModelMatrix *= Matrix4.CreateTranslation(X, -(Y + Height), 0);
+            }
         }
     }
 }
--- VengineX/UI/Fonts/BitmapFont.cs
+++ VengineX/UI/Fonts/BitmapFont.cs
@@ -9,7 +9,9 @@ using System.Threading.Tasks;
 using VengineX.Debugging.Logging;
 using VengineX.Graphics.Rendering;
 using VengineX.Graphics.Rendering.Buffers;
+using VengineX.Graphics.Rendering.Shaders;
 using VengineX.Graphics.Rendering.Textures;
+using VengineX.Graphics.Rendering.UnitModels;
 using VengineX.Graphics.Rendering.Vertices;
 using VengineX.Resources;
 using VengineX.Utils;
@@ -33,16 +35,22 @@ namespace VengineX.UI.Fonts
         /// </summary>
         public Texture2D TextureAtlas { get; private set; }
 
+        /// <summary>
+        /// The size (height) of the font in pixels.
+        /// </summary>
+        public float Size { get; private set; }
 
-        //private Dictionary<byte, Character> _characters;
-        public Dictionary<byte, Texture2D?> _tex;
+        public Dictionary<byte, Texture2D?> textures;
+
+
+        private readonly Dictionary<byte, Character> _characters;
 
         /// <summary>
         /// Creates an empty BitmapFont object (for <see cref="ResourceManager"/>).
         /// </summary>
         public BitmapFont()
         {
-            //_characters = new Dictionary<byte, Character>();
+            _characters = new Dictionary<byte, Character>();
         }
 
 
@@ -53,6 +61,7 @@ namespace VengineX.UI.Fonts
         public void Load(ref ILoadingParameters loadingParameters)
         {
             BitmapFontLoadingParameters parameters = (BitmapFontLoadingParameters)loadingParameters;
+            Size = parameters.Size;
 
             unsafe
             {
@@ -63,14 +72,23 @@ namespace VengineX.UI.Fonts
                     parameters.ToCharCode,
                     parameters.Size);
 
+
+
                 int length = parameters.ToCharCode - parameters.FromCharCode;
 
                 // Create a texture2d for each character (if bitmap data is null, texture will be null aswell)
                 //Dictionary<byte, Texture2D?> textures = CreateTextures(glyphs, length);
-                _tex = CreateTextures(glyphs, length);
+                textures = CreateTextures(glyphs, length);
 
                 // Render all the textures into a combined texture2d atlas.
-                CreateAtlas(glyphs, length, parameters.Size, _tex);
+                CreateAtlas(glyphs, length, parameters.Size, textures);
+
+
+                //// Dispose temporary textures
+                //foreach (Texture2D? texture in textures.Values)
+                //{
+                //    texture?.Dispose();
+                //}
 
                 // Free glyphs
                 FreeTypeWrapper.FreeGlyphs(glyphs, length);
@@ -138,11 +156,89 @@ namespace VengineX.UI.Fonts
             Framebuffer2D fb = new Framebuffer2D(textureSize, textureSize, PixelInternalFormat.R8, PixelFormat.Red, false);
             TextureAtlas = fb.DetachTexture();
 
-            // Framebuffer no longer needed.
+            // Get shader, create quad and uniforms required for rendering
+            Quad quad = new Quad();
+            Shader imageShader = ResourceManager.GetResource<Shader>("shader.image");
+            Matrix4 proj = Matrix4.CreateOrthographicOffCenter(0, textureSize, -textureSize, 0, -1.0f, 1.0f);
+            Matrix4 view = Matrix4.Identity;
+            Vector4 color = Vector4.One;
+
+            // Render all the textures
+            fb.Bind();
+            GL.ClearColor(0, 0, 0, 1);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            imageShader.Bind();
+            imageShader.SetUniformMat4("P", ref proj);
+            imageShader.SetUniformMat4("V", ref view);
+            imageShader.SetUniformVec4("uColor", ref color);
+
+
+            int rowSpaceUsed = 0;
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < length; i++)
+            {
+                FreeTypeGlyph glyph = glyphs[i];
+
+                // Update model matrix.
+                Matrix4 m = Matrix4.Identity;
+                m *= Matrix4.CreateScale(glyph.width / 2f, glyph.height / -2f, 0);
+                m *= Matrix4.CreateTranslation(glyph.width / 2f + x, -(glyph.height / 2f + y), 0);
+                imageShader.SetUniformMat4("M", ref m);
+
+                // Bind the glyphs texture
+                textures[glyph.charCode]?.Bind();
+
+                // Render on quad.
+                quad.Render();
+
+
+                float w = glyph.width;
+                float h = glyph.height;
+
+                // Calculate uvs: upper-left, upper-right, lower-left, lower-right.
+                Vector2[] uvs = new Vector2[]
+                {
+                    new Vector2((float)x / textureSize,         (float)(y + h) / textureSize),
+                    new Vector2((float)(x + w) / textureSize,   (float)(y + h) / textureSize),
+                    new Vector2((float)x / textureSize,         (float)y / textureSize),
+                    new Vector2((float)(x + w) / textureSize,   (float)y / textureSize),
+                };
+
+
+                // Create Character and add to dict.
+                _characters.Add(glyph.charCode, new Character()
+                {
+                    Size = new Vector2i(glyph.width, glyph.height),
+                    Bearing = new Vector2i(glyph.left, glyph.top),
+                    Advance = glyph.advance,
+                    UVs = uvs,
+                });
+
+
+                // Calculate next x and y.
+                rowSpaceUsed += size;
+                if (rowSpaceUsed + size > textureSize)
+                {
+                    // No more space in this row.
+                    y += size;
+                    x = 0;
+                    rowSpaceUsed = 0;
+                }
+                else
+                {
+                    x += size;
+                }
+            }
+
+
+            fb.Unbind();
+
+
+            // Dispose stuff that is no longer needed.
             fb.Dispose();
-
-            Console.WriteLine($"{length} glyphs, each {size}x{size}: {textureSize}x{textureSize}");
-
+            quad.Dispose();
         }
 
 
@@ -151,16 +247,82 @@ namespace VengineX.UI.Fonts
         /// </summary>
         public Mesh<UIVertex> CreateMesh(string text)
         {
-            throw new NotImplementedException();
+            UIVertex[] vertices = new UIVertex[text.Length * 4];
+            uint[] indices = new uint[text.Length * 6];
+
+            uint x = 0;
+            uint vertIndex = 0;
+            uint indIndex = 0;
+
+            foreach (char c in text)
+            {
+                Character ch = _characters[(byte)c];
+
+                // Dimensions
+                float xPos = x + ch.Bearing.X;
+                float yPos = -(ch.Size.Y - ch.Bearing.Y);
+
+                float w = ch.Size.X;
+                float h = ch.Size.Y;
+
+                // Vertices
+                vertices[vertIndex + 0] = new UIVertex()
+                {
+                    position = new Vector3(xPos, yPos, 0),
+                    uv = ch.UVs[0],
+                };
+                vertices[vertIndex + 1] = new UIVertex()
+                {
+                    position = new Vector3(xPos + w, yPos, 0),
+                    uv = ch.UVs[1],
+                };
+                vertices[vertIndex + 2] = new UIVertex()
+                {
+                    position = new Vector3(xPos, yPos + h, 0),
+                    uv = ch.UVs[2],
+                };
+                vertices[vertIndex + 3] = new UIVertex()
+                {
+                    position = new Vector3(xPos + w, yPos + h, 0),
+                    uv = ch.UVs[3],
+                };
+
+                Console.WriteLine(vertices[vertIndex + 0].position + ", " + vertices[vertIndex + 0].uv);
+                Console.WriteLine(vertices[vertIndex + 1].position + ", " + vertices[vertIndex + 1].uv);
+                Console.WriteLine(vertices[vertIndex + 2].position + ", " + vertices[vertIndex + 2].uv);
+                Console.WriteLine(vertices[vertIndex + 3].position + ", " + vertices[vertIndex + 3].uv);
+
+                // Indices
+                indices[indIndex + 0] = vertIndex + 0;
+                indices[indIndex + 1] = vertIndex + 1;
+                indices[indIndex + 2] = vertIndex + 2;
+
+                indices[indIndex + 3] = vertIndex + 2;
+                indices[indIndex + 4] = vertIndex + 1;
+                indices[indIndex + 5] = vertIndex + 3;
+
+                vertIndex += 4;
+                indIndex += 6;
+
+                x += ch.Advance >> 6;
+            }
+
+            return new Mesh<UIVertex>(Vector3.Zero, vertices, indices);
         }
 
 
         /// <summary>
-        /// Calculates the width of the given text.
+        /// Calculates the width of the given text (unscaled).
         /// </summary>
         public float CalculateWidth(string text)
         {
-            throw new NotImplementedException();
+            float width = 0;
+            foreach (char c in text)
+            {
+                width += _characters[(byte)c].Advance;
+
+            }
+            return width;
         }
 
 
@@ -177,10 +339,12 @@ namespace VengineX.UI.Fonts
             {
                 if (disposing)
                 {
-                    // Dispose managed
-                    foreach (Texture2D tex in _tex.Values)
+                    TextureAtlas.Dispose();
+
+                    // Dispose temporary textures
+                    foreach (Texture2D? texture in textures.Values)
                     {
-                        tex.Dispose();
+                        texture?.Dispose();
                     }
                 }
 
