62e08612ae49fe7a1cc73e454f8f963ff390e3b1
BlakkM9 <julian.desloges@gmail.com>
2022-03-14 17:25:42 +0100

    Added unmanaged array struct.

---
+++ VengineX/Utils/UnmanagedArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VengineX.Utils
+{
+    public unsafe struct UnmanagedArray<T> where T : unmanaged
+    {
+        /// <summary>
+        /// Pointer to the first element of the array (IntPtr)
+        /// </summary>
+        public IntPtr Pointer { get; }
+
+        /// <summary>
+        /// Pointer to the first element of the array (T*)
+        /// </summary>
+        public T* NativePointer { get; }
+
+
+        /// <summary>
+        /// Elements in the unmanaged array
+        /// </summary>
+        public int Length { get; }
+
+        /// <summary>
+        /// Access to the elements. Does not check for bounds.
+        /// </summary>
+        /// <returns>Element at given index i.</returns>
+        public T this[int i]
+        {
+            get { return *(NativePointer + i); }
+            set { *(NativePointer + i) = value; }
+        }
+
+        /// <summary>
+        /// Access to the elements. Does not check for bounds.
+        /// </summary>
+        /// <returns>Element at given index i.</returns>
+        public T this[uint i]
+        {
+            get { return *(NativePointer + i); }
+            set { *(NativePointer + i) = value; }
+        }
+
+        /// <summary>
+        /// Creates a new unmanaged array
+        /// </summary>
+        /// <param name="length">Length (items) of the array</param>
+        public UnmanagedArray(int length)
+        {
+            Pointer = Marshal.AllocHGlobal(sizeof(T) * length);
+            NativePointer = (T*)Pointer;
+            Length = length;
+        }
+
+
+        public UnmanagedArray(T* dataPtr, int length)
+        {
+            Pointer = (IntPtr)dataPtr;
+            NativePointer = dataPtr;
+            Length = length;
+        }
+
+
+        /// <summary>
+        /// Free's the memory of this array.
+        /// Needs to be called to prevent memory leaks
+        /// </summary>
+        public void Free()
+        {
+            Marshal.FreeHGlobal(Pointer);
+        }
+    }
+}
