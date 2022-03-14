35c91ca8843a0c0edd712bf2af80217d18e6c051
BlakkM9 <julian.desloges@gmail.com>
2022-03-14 07:52:57 +0100

    Adjusted to match with other ui shaders

--- VengineX/_res/shaders/image.fs.glsl
+++ VengineX/_res/shaders/image.fs.glsl
@@ -1,7 +1,7 @@
 ﻿#version 450 core
 
 uniform sampler2D uTexture;
-uniform vec4 uTint;
+uniform vec4 uColor;
 
 in vec2 uv;
 layout(location = 0) out vec4 fColor;
@@ -9,7 +9,5 @@ layout(location = 0) out vec4 fColor;
 void main()
 {
 	vec4 sampled = texture(uTexture, uv);
-	vec3 color = clamp(sampled.rgb + uTint.rgb, 0.0, 1.0);
-	fColor = vec4(color, sampled.a * uTint.a);
-//	fColor = uTint;
+	fColor = uColor * sampled;
 }
