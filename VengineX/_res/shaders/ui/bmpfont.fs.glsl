add79a3bdc7c17178aee9cf090d0005611649def
BlakkM9 <julian.desloges@gmail.com>
2022-03-14 20:50:31 +0100

    Removed outcommonted code

--- VengineX/_res/shaders/ui/bmpfont.fs.glsl
+++ VengineX/_res/shaders/ui/bmpfont.fs.glsl
@@ -9,8 +9,6 @@ layout(location = 0) out vec4 fColor;
 void main()
 {
 	vec4 sampled = vec4(1.0, 1.0, 1.0, texture(uTexture, vec2(uv.x, 1 - uv.y)).r);
-	fColor = clamp((uColor * sampled) + vec4(0.2), 0, 1);
-//	fColor = vec4(uv, 0, 1);
-	fColor = sampled;
+	fColor = uColor * sampled;
 //	fColor = vec4(1);
 }
