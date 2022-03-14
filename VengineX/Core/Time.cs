a6e2061d1bc0aeb756a252788a07abb622004af5
BlakkM9 <julian.desloges@gmail.com>
2022-03-14 17:25:52 +0100

    Added average ups and fps

--- VengineX/Core/Time.cs
+++ VengineX/Core/Time.cs
@@ -34,6 +34,38 @@ namespace VengineX.Core
         /// </summary>
         public static double CurrentUPS { get; private set; } = 0;
 
+        /// <summary>
+        /// Averaged ups over <see cref="UPSSampleCount"/> frames
+        /// </summary>
+        public static double AverageUPS { get; private set; }
+
+
+        /// <summary>
+        /// Amount of frames to sample for average fps.
+        /// </summary>
+        public static int UPSSampleCount
+        {
+            get { return _upsSampleCount; }
+            set
+            {
+                _upsSampleCount = value;
+                _upsSamples = new double[_upsSampleCount];
+            }
+        }
+        private static int _upsSampleCount = 100;
+
+
+        /// <summary>
+        /// Current sampled fps
+        /// </summary>
+        private static double[] _upsSamples = new double[_upsSampleCount];
+
+
+        /// <summary>
+        /// Total render frames
+        /// </summary>
+        private static int _updateFrameCount = 0;
+
 
         /// <summary>
         /// Time elapsed since last render (seconds)
@@ -48,6 +80,39 @@ namespace VengineX.Core
 
 
         /// <summary>
+        /// Averaged fps over <see cref="FPSSampleCount"/> frames
+        /// </summary>
+        public static double AverageFPS { get; private set; }
+
+
+        /// <summary>
+        /// Amount of frames to sample for average fps.
+        /// </summary>
+        public static int FPSSampleCount
+        {
+            get { return _fpsSampleCount; }
+            set
+            {
+                _fpsSampleCount = value;
+                _fpsSamples = new double[_fpsSampleCount];
+            }
+        }
+        private static int _fpsSampleCount = 100;
+
+
+        /// <summary>
+        /// Current sampled fps
+        /// </summary>
+        private static double[] _fpsSamples = new double[_fpsSampleCount];
+
+
+
+        /// <summary>
+        /// Total render frames
+        /// </summary>
+        private static int _renderFrameCount = 0;
+
+        /// <summary>
         /// Updates all the update time properties.<br/>
         /// Do not call this function except in <see cref="Game{T}.Window_UpdateFrame(OpenTK.Windowing.Common.FrameEventArgs)"/>.
         /// </summary>
@@ -57,6 +122,10 @@ namespace VengineX.Core
             SinTotal = Math.Sin(Total);
             DeltaUpdate = delta;
             CurrentUPS = 1.0 / DeltaUpdate;
+
+            _updateFrameCount++;
+            _upsSamples[_updateFrameCount % UPSSampleCount] = CurrentUPS;
+            AverageUPS = (double)_upsSamples.Sum() / UPSSampleCount;
         }
 
 
@@ -68,6 +137,10 @@ namespace VengineX.Core
         {
             DeltaRender = delta;
             CurrentFPS = 1.0 / DeltaRender;
+
+            _renderFrameCount++;
+            _fpsSamples[_renderFrameCount % FPSSampleCount] = CurrentFPS;
+            AverageFPS = (double)_fpsSamples.Sum() / FPSSampleCount;
         }
     }
 }
