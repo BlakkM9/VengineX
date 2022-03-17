de2380eb31b15dab96982987e18c8f9a120e73c6
BlakkM9 <julian.desloges@gmail.com>
2022-03-17 23:48:05 +0100

    reworked image shader so box shader / element is no longer needed

--- VengineX/_res/shaders/ui/image.fs.glsl
+++ VengineX/_res/shaders/ui/image.fs.glsl
@@ -2,6 +2,7 @@
 
 uniform sampler2D uTexture;
 uniform vec4 uColor;
+uniform vec4 uTint;
 
 in vec2 uv;
 layout(location = 0) out vec4 fColor;
@@ -9,5 +10,5 @@ layout(location = 0) out vec4 fColor;
 void main()
 {
 	vec4 sampled = texture(uTexture, uv);
-	fColor = uColor * sampled;
+	fColor = clamp(uTint * sampled + uColor, 0.0, 1.0);
 }
