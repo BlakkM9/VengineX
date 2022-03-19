66ca5344446e72b6b1301ec9dfd0e5d3dd060871
BlakkM9 <julian.desloges@gmail.com>
2022-03-19 21:32:09 +0100

    Found better name for this class. Still not happy.

---
+++ VengineX/UI/LoadableUI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VengineX.UI.Elements;
+using VengineX.UI.Elements.Panels;
+using VengineX.UI.Layouts;
+using VengineX.UI.Serialization;
+
+namespace VengineX.UI
+{
+    /// <summary>
+    /// Base class for a group of UIElements that can be loaded from an XML file.<br/>
+    /// This will be the root element of the loaded UI. For loading the UI use <see cref="UISerializer.LoadFromXML{T}(UIElement, string)"/>.
+    /// </summary>
+    public abstract class LoadableUI : UIElement
+    {
+        protected LoadableUI(UIElement parent) : base(parent) { }
+
+        /// <summary>
+        /// This function is calld when the <see cref="LoadableUI"/> was initialized after loading.
+        /// </summary>
+        protected abstract void Initialized();
+    }
+}
