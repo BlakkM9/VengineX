5ef74c50c768be6bb650e6c813ddec4abe5548b9
BlakkM9 <julian.desloges@gmail.com>
2022-03-19 06:04:11 +0100

    Added panel for align layout

---
+++ VengineX/UI/Elements/Panels/AlignPane.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VengineX.UI.Layouts;
+
+namespace VengineX.UI.Elements.Panels
+{
+    /// <summary>
+    /// Pane that uses <see cref="AlignLayout"/>
+    /// </summary>
+    public class AlignPane : UIElement
+    {
+        /// <summary>
+        /// Creates a new align pane.
+        /// </summary>
+        public AlignPane(
+            UIElement parent,
+            HorizontalAlignment horizontalAlignment,
+            VerticalAlignment verticalAlignment) : base(parent)
+        {
+            Layout = new AlignLayout(horizontalAlignment, verticalAlignment);
+        }
+    }
+}
