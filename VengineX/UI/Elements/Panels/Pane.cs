76ca1b54f927e84a2ef87e63377516deaa571aaa
BlakkM9 <julian.desloges@gmail.com>
2022-03-19 08:11:34 +0100

    Added pane without layout (can be used as container)

---
+++ VengineX/UI/Elements/Panels/Pane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VengineX.UI.Elements.Panels
+{
+    /// <summary>
+    /// Most basic pane that does not have any specified layout.
+    /// </summary>
+    public class Pane : UIElement
+    {
+        public Pane(UIElement parent) : base(parent) { }
+    }
+}
