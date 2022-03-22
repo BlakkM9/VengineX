ec23509fb0f97592d4f8b220153f07be5c140ea4
BlakkM9 <julian.desloges@gmail.com>
2022-03-22 04:52:26 +0100

    Added Array2D texture

---
+++ VengineX/Graphics/Rendering/Textures/Texture2DArray.cs
@@ -0,0 +1,54 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VengineX.Resources;
+
+namespace VengineX.Graphics.Rendering.Textures
+{
+    public class Texture2DArray : Texture
+    {
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public int Layers { get; private set; }
+
+
+
+        /// <summary>
+        /// Creates a empty texture 2d object for being loaded via <see cref="ResourceManager"/>
+        /// </summary>
+        public Texture2DArray() : base(TextureTarget.Texture2DArray) { }
+
+        /// <summary>
+        /// Creates a new texture 2d array from given parameters.
+        /// </summary>
+        public Texture2DArray(ref Texture2DArrayParameters parameters) : this()
+        {
+            Width = parameters.Width;
+            Height = parameters.Height;
+            Layers = parameters.Layers;
+
+            // Min/Mag
+            GL.TextureParameter(Handle, TextureParameterName.TextureMinFilter, (int)parameters.MinFilter);
+            GL.TextureParameter(Handle, TextureParameterName.TextureMagFilter, (int)parameters.MagFilter);
+
+            // Wrap S/T/R
+            GL.TextureParameter(Handle, TextureParameterName.TextureWrapS, (int)parameters.WrapModeS);
+            GL.TextureParameter(Handle, TextureParameterName.TextureWrapT, (int)parameters.WrapModeT);
+            GL.TextureParameter(Handle, TextureParameterName.TextureWrapR, (int)parameters.WrapModeR);
+
+            GL.TextureStorage3D(Handle, 1, parameters.InternalFormat, Width, Height, Layers);
+            GL.TextureSubImage3D(Handle, 0, 0, 0, 0, Width, Height, Layers, parameters.PixelFormat, parameters.PixelType, parameters.PixelData);
+
+            // Generate mipmaps
+            if (parameters.GenerateMipmaps)
+            {
+                GL.GenerateTextureMipmap(Handle);
+            }
+        }
+    }
+}
---
+++ VengineX/Graphics/Rendering/Textures/Texture2DArrayParameters.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VengineX.Graphics.Rendering.Textures
+{
+    public struct Texture2DArrayParameters
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Layers { get; set; }
+
+        public TextureMinFilter MinFilter { get; set; }
+        public TextureMagFilter MagFilter { get; set; }
+
+        public TextureWrapMode WrapModeS { get; set; }
+        public TextureWrapMode WrapModeT { get; set; }
+        public TextureWrapMode WrapModeR { get; set; }
+
+        public SizedInternalFormat InternalFormat { get; set; }
+        public PixelFormat PixelFormat { get; set; }
+        public PixelType PixelType { get; set; }
+        public bool GenerateMipmaps { get; set; }
+
+        public IntPtr PixelData { get; set; }
+
+    }
+}
