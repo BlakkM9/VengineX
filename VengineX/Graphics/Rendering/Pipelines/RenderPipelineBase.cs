d951a794b139d8896a0a6c23b879727d2d486c1a
BlakkM9 <julian.desloges@gmail.com>
2022-03-25 14:08:46 +0100

    Removed OverlayUI

--- VengineX/Graphics/Rendering/Pipelines/RenderPipelineBase.cs
+++ VengineX/Graphics/Rendering/Pipelines/RenderPipelineBase.cs
@@ -7,17 +7,12 @@ namespace VengineX.Graphics.Rendering.Pipelines
     /// <summary>
     /// Base class for rendering pipelines.
     /// </summary>
-    public abstract class RenderPipelineBase : IRenderPipeline, IDisposable
+    public abstract class RenderPipelineBase : IRenderPipeline
     {
         /// <summary>
         /// The main camera for this pipeline.
         /// </summary>
         public virtual Camera Camera { get; protected set; }
-
-        /// <summary>
-        /// The overlay ui in this pipeline.
-        /// </summary>
-        public virtual Canvas OverlayUI { get; protected set; }
 
         /// <summary>
         /// The clear color for this pipeline.
@@ -28,62 +23,5 @@ namespace VengineX.Graphics.Rendering.Pipelines
         /// Renders this pipelines content.
         /// </summary>
         public abstract void Render();
-
-
-        /// <summary>
-        /// Dispose managed state (managed objects) here.
-        /// </summary>
-        public abstract void DisposeManaged();
-
-
-        /// <summary>
-        /// Free unmanaged resources (unmanaged objects) and<br/>
-        /// set large fields to null here.
-        /// </summary>
-        public abstract void DisposeUnmanaged();
-
-        public abstract void AddRenderable(Material material, IRenderable renderable);
-
-        #region IDisposable
-
-        private bool _disposedValue;
-
-        /// <summary>
-        /// Disposable pattern.
-        /// </summary>
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposedValue)
-            {
-                if (disposing)
-                {
-                    DisposeManaged();
-                }
-
-                DisposeUnmanaged();
-                _disposedValue = true;
-            }
-        }
-
-        /// <summary>
-        /// Disposable pattern.
-        /// </summary>
-        ~RenderPipelineBase()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: false);
-        }
-
-        /// <summary>
-        /// Disposable pattern.
-        /// </summary>
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-
-        #endregion
     }
 }
