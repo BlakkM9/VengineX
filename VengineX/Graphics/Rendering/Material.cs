7a010b752f00bb984c3791974b845cdbd1e6549a
BlakkM9 <julian.desloges@gmail.com>
2022-03-25 14:09:17 +0100

    Reworked material system

--- VengineX/Graphics/Rendering/Material.cs
+++ VengineX/Graphics/Rendering/Material.cs
@@ -1,78 +1,111 @@
 ﻿using OpenTK.Mathematics;
+using VengineX.Debugging.Logging;
 using VengineX.Graphics.Rendering.Shaders;
 using VengineX.Graphics.Rendering.Textures;
 
 namespace VengineX.Graphics.Rendering
 {
-    public class Material : IBindable, IEquatable<Material>
+    /// <summary>
+    /// A material groups a shader and materials together.
+    /// </summary>
+    public class Material : IBindable
     {
+        /// <summary>
+        /// The shader used by this material.
+        /// </summary>
         public Shader Shader { get; }
-        public Texture2D[] Textures { get; }
 
-        public Uniform ModelMatrixUniform { get; }
-        public Uniform ViewMatrixUniform { get; }
-        public Uniform ProjectionMatrixUniform { get; }
+        /// <summary>
+        /// The textures of this material with the uniform name as key.
+        /// </summary>
+        private readonly Dictionary<string, Texture> _textures;
+
+        /// <summary>
+        /// Array that manages the texture slot for each texture.<br/>
+        /// The index represents the texture slot to use for the texture on that index.
+        /// </summary>
+        private readonly Texture?[] _textureSlots;
 
 
-        public Material(Shader shader, params Texture2D[] textures)
+        /// <summary>
+        /// Creates a new empty material
+        /// </summary>
+        /// <param name="shader"></param>
+        public Material(Shader shader)
         {
             Shader = shader;
-            ModelMatrixUniform = Shader.GetUniform("M");
-            ViewMatrixUniform = Shader.GetUniform("V");
-            ProjectionMatrixUniform = Shader.GetUniform("P");
+            _textures = new Dictionary<string, Texture>();
+            _textureSlots = new Texture[32];
+        }
 
-            Textures = new Texture2D[32];
 
-            for (int i = 0; i < textures.Length; i++)
+        /// <summary>
+        /// Adds a texture with given uniform name to this material.
+        /// </summary>
+        public void AddTexture(string uniformName, Texture texture)
+        {
+            // Add to dictionary for user access
+            _textures.Add(uniformName, texture);
+
+            // Find next available texture slot
+            int slot = -1;
+            for (int i = 0; i < _textureSlots.Length; i++)
             {
-                Textures[i] = textures[i];
+                if (_textureSlots[i] == null)
+                {
+                    _textureSlots[i] = texture;
+                    slot = i;
+                    break;
+                }
+            }
+
+            if (slot == -1)
+            {
+                Logger.Log(Severity.Error, "No free texture slot found!");
+                return;
+            }
+
+            // Set texture uniform to assigned slot
+            Shader.GetUniform(uniformName).Set1(slot);
+        }
+
+
+        /// <summary>
+        /// Removes the texture for given uniform name.
+        /// </summary>
+        public void RemoveTexture(string uniformName)
+        {
+            // Remove from dict
+            _textures.Remove(uniformName);
+
+            // Remove from slots
+            for (int i = 0; i < _textureSlots.Length; i++)
+            {
+                _textureSlots[i] = null;
             }
         }
 
 
-        public void UpdateCameraMatrixUniforms(ref Matrix4 viewMatrix, ref Matrix4 projectionMatrix)
-        {
-            //Shader.SetUniformMat4(ViewMatrixUniform, ref viewMatrix);
-            //Shader.SetUniformMat4(ProjectionMatrixUniform, ref projectionMatrix);
-            ProjectionMatrixUniform.SetMat4(ref projectionMatrix);
-            ViewMatrixUniform.SetMat4(ref viewMatrix);
-        }
-
-
-        public virtual void UpdateModelMatrix(ref Matrix4 modelMatrix)
-        {
-            ModelMatrixUniform.SetMat4(ref modelMatrix);
-        }
-
-
+        /// <summary>
+        /// Binds the shader and all the textures of this material to the current<br/>
+        /// opengl renderer state.
+        /// </summary>
         public void Bind()
         {
+            // Bind the used shader
             Shader.Bind();
 
-            for (uint i = 0; i < Textures.Length; i++)
+            // Bind each texture to previously assinged slot
+            for (uint i = 0; i < _textureSlots.Length; i++)
             {
-                Textures[i]?.Bind(i);
+                _textureSlots[i]?.Bind(i);
             }
         }
 
 
         public void Unbind()
         {
-
-        }
-
-
-        public bool Equals(Material? other)
-        {
-            if (other == this) { return true; }
-            if (other == null) { return false; }
-
-            for (int i = 0; i < Textures.Length; i++)
-            {
-                if (other.Textures[i].Handle != Textures[i].Handle) { return false; }
-            }
-
-            return Shader.Handle == other.Shader.Handle;
+            throw new NotImplementedException();
         }
     }
 }
