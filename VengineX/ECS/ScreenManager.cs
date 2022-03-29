dac8381bf853db79a47ee13171b0b288a2ea9c6a
BlakkM9 <julian.desloges@gmail.com>
2022-03-29 15:19:55 +0200

    Reworked screen manager to be a FSM

--- VengineX/ECS/ScreenManager.cs
+++ VengineX/ECS/ScreenManager.cs
@@ -2,20 +2,75 @@
 {
     public class ScreenManager
     {
+        /// <summary>
+        /// Handler for the <see cref="ScreenChanged"/> event.
+        /// </summary>
+        public delegate void ScreenChangedHandler(Screen? previousScreen, Screen newScreen);
+
+        /// <summary>
+        /// Occurs when the screen changed.
+        /// </summary>
+        public event ScreenChangedHandler? ScreenChanged;
+
+        /// <summary>
+        /// Dictionary holding all the screens with their type as key.
+        /// </summary>
+        private Dictionary<Type, Screen> _screens;
+
 
         /// <summary>
         /// Screen that is currently active
         /// </summary>
-        public IScreen? CurrentScreen
+        public Screen? CurrentScreen { get => _currentScreen; private set => _currentScreen = value; }
+        private Screen? _currentScreen;
+
+
+        /// <summary>
+        /// Creates a new screen manager.
+        /// </summary>
+        public ScreenManager() { 
+            _screens = new Dictionary<Type, Screen>();
+        }
+
+
+        /// <summary>
+        /// Adds a screen to the screen manager.
+        /// </summary>
+        public void AddScreen(Screen screen)
         {
-            get => _currentScreen;
-            set
+            _screens.Add(screen.GetType(), screen);
+        }
+
+
+        /// <summary>
+        /// Sets the current screen to the given screen type.
+        /// </summary>
+        public void SetScreen<T>() where T : Screen
+        {
+            // Get the screen from dictionary
+            if (_screens.TryGetValue(typeof(T), out Screen? newScreen))
             {
                 CurrentScreen?.Unload();
-                _currentScreen = value;
-                CurrentScreen?.Load();
+
+                Screen? previousScreen = CurrentScreen;
+                CurrentScreen = newScreen;
+
+                newScreen.Load();
+
+                ScreenChanged?.Invoke(previousScreen, newScreen);
             }
         }
-        protected IScreen? _currentScreen;
+
+
+        /// <summary>
+        /// Updates the current screen.
+        /// </summary>
+        public void Update(double delta) => CurrentScreen?.Update(delta);
+
+
+        /// <summary>
+        /// Renders the current screen.
+        /// </summary>
+        public void Render(double delta) => CurrentScreen?.Render(delta);
     }
 }
