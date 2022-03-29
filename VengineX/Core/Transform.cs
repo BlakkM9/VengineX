d96953f6c23cc8590e3769060423add60dfee1f6
BlakkM9 <julian.desloges@gmail.com>
2022-03-29 15:17:43 +0200

    Added transform component, holding and controlling a model matrix

---
+++ VengineX/Core/Transform.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VengineX.ECS;
+
+namespace VengineX.Core
+{
+    public class Transform : Component
+    {
+        public ref Matrix4 ModelMatrix { get => ref _modelMatrix; }
+        private Matrix4 _modelMatrix;
+
+        /// <summary>
+        /// Creates a new transfrom with the matrix set to identity.
+        /// </summary>
+        public Transform()
+        {
+            _modelMatrix = Matrix4.Identity;
+        }
+    }
+}
