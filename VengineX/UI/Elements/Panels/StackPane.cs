3e12837a90e71047f5eb374e8c449af8c3e015f6
BlakkM9 <julian.desloges@gmail.com>
2022-03-29 14:22:29 +0200

    Refactored so it can be used in ui xml files

--- VengineX/UI/Elements/Panels/StackPane.cs
+++ VengineX/UI/Elements/Panels/StackPane.cs
@@ -9,16 +9,76 @@ namespace VengineX.UI.Elements.Panels
     public class StackPane : Pane
     {
         /// <summary>
+        /// Horizontal layout of the children in this pane.
+        /// </summary>
+        public HorizontalAlignment HorizontalAlignment
+        {
+            get => _horizontalAlignment;
+            set
+            {
+                _horizontalAlignment = value;
+                Layout = new StackLayout(_horizontalAlignment, _verticalAlignment, _orientation, _spacing);
+            }
+        }
+        /// <summary>
+        /// Internal field for <see cref="HorizontalAlignment"/>.
+        /// </summary>
+        protected HorizontalAlignment _horizontalAlignment;
+
+        /// <summary>
+        /// Vertical layout of the children in this pane.
+        /// </summary>
+        public VerticalAlignment VerticalAlignment
+        {
+            get => _verticalAlignment;
+            set
+            {
+                _verticalAlignment = value;
+                Layout = new StackLayout(_horizontalAlignment, _verticalAlignment, _orientation, _spacing);
+            }
+        }
+        /// <summary>
+        /// Internal field for <see cref="VerticalAlignment"/>.
+        /// </summary>
+        protected VerticalAlignment _verticalAlignment;
+
+        /// <summary>
+        /// The orientation of children in this pane.
+        /// </summary>
+        public Orientation Orientation
+        {
+            get => _orientation;
+            set
+            {
+                _orientation = value;
+                Layout = new StackLayout(_horizontalAlignment, _verticalAlignment, _orientation, _spacing);
+            }
+        }
+        /// <summary>
+        /// Internal field for <see cref="Orientation"/>.
+        /// </summary>
+        protected Orientation _orientation;
+
+        /// <summary>
+        /// Spacing between the children in this pane.
+        /// </summary>
+        public float Spacing
+        {
+            get => _spacing;
+            set
+            {
+                _spacing = value;
+                Layout = new StackLayout(_horizontalAlignment, _verticalAlignment, _orientation, _spacing);
+            }
+        }
+        /// <summary>
+        /// Internal field for <see cref="Spacing"/>.
+        /// </summary>
+        protected float _spacing;
+
+        /// <summary>
         /// Creates a new stack pane.
         /// </summary>
-        public StackPane(
-            Element parent,
-            HorizontalAlignment horizontalAlignment,
-            VerticalAlignment verticalAlignment,
-            Orientation orientation, float spacing)
-            : base(parent)
-        {
-            Layout = new StackLayout(horizontalAlignment, verticalAlignment, orientation, spacing);
-        }
+        public StackPane(Element parent) : base(parent) { }
     }
 }
