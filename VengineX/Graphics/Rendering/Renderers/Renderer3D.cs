7a677e9e062deb065ebf6c38c9822e1b3ae29c30
BlakkM9 <julian.desloges@gmail.com>
2022-03-31 21:20:54 +0200

    Added docs

--- VengineX/Graphics/Rendering/Renderers/Renderer3D.cs
+++ VengineX/Graphics/Rendering/Renderers/Renderer3D.cs
@@ -9,18 +9,31 @@ using VengineX.Graphics.Rendering.Meshes;
 
 namespace VengineX.Graphics.Rendering.Renderers
 {
+    /// <summary>
+    /// Class that handles rendering of mesh components.<br/>
+    /// Sorted by materials and then rendered.
+    /// </summary>
     public class Renderer3D
     {
+        private readonly Dictionary<Material, List<MeshComponent>> _meshes;
+        private Camera? _camera;
 
-        private Dictionary<Material, List<MeshComponent>> _meshes;
-        private Camera _camera;
 
+        /// <summary>
+        /// Creates a new renderer for 3D meshes.
+        /// </summary>
         public Renderer3D()
         {
             _meshes = new Dictionary<Material, List<MeshComponent>>();
         }
 
 
+        /// <summary>
+        /// Begins the renderer.<br/>
+        /// Submit all mesh components between <see cref="Begin(Camera)"/> and <see cref="End"/><br/>
+        /// and render them by calling <see cref="Flush"/>.
+        /// </summary>
+        /// <param name="camera">Camera that provides view and projection matrix for the meshes.</param>
         public void Begin(Camera camera)
         {
             foreach (List<MeshComponent> meshList in _meshes.Values)
@@ -32,7 +45,9 @@ namespace VengineX.Graphics.Rendering.Renderers
         }
 
 
-
+        /// <summary>
+        /// Submits the given <see cref="MeshComponent"/> to the renderer.
+        /// </summary>
         public void Submit(MeshComponent mesh)
         {
             if (_meshes.ContainsKey(mesh.Material))
@@ -47,12 +62,19 @@ namespace VengineX.Graphics.Rendering.Renderers
         }
 
 
+        /// <summary>
+        /// Ends the submitting.
+        /// </summary>
         public void End() { }
 
 
+        /// <summary>
+        /// Renders all the <see cref="MeshComponent"/>s in the renderer.
+        /// </summary>
         public void Flush()
         {
-            
+            if (_camera == null) { return; }
+
             foreach (KeyValuePair<Material, List<MeshComponent>> kvp in _meshes)
             {
                 kvp.Key.Bind();
