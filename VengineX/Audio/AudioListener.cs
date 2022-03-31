b75887d5583827d67449b134c0898d2eea37cd14
BlakkM9 <julian.desloges@gmail.com>
2022-03-31 21:18:44 +0200

    Added skeletton for audio

---
+++ VengineX/Audio/AudioListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VengineX.ECS;
+
+namespace VengineX.Audio
+{
+    public class AudioListener : Component
+    {
+        public AudioListener() : base(typeof(AudioListener)) { }
+    }
+}
---
+++ VengineX/Audio/AudioSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VengineX.ECS;
+
+namespace VengineX.Audio
+{
+    public class AudioSource : Component
+    {
+        public AudioSource() : base(typeof(AudioSource)) { }
+    }
+}
