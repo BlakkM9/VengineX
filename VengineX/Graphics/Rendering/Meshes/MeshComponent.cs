02fbe255c84aea85f8a864d1fead153e92c61b8d
BlakkM9 <julian.desloges@gmail.com>
2022-03-31 21:20:28 +0200

    MeshComponent is now using the transform component of the parent entity

--- VengineX/Graphics/Rendering/Meshes/MeshComponent.cs
+++ VengineX/Graphics/Rendering/Meshes/MeshComponent.cs
@@ -4,25 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VengineX.Core;
+using VengineX.Debugging.Logging;
 using VengineX.ECS;
 
 namespace VengineX.Graphics.Rendering.Meshes
 {
+    /// <summary>
+    /// The component for meshes that are rendered in the screen.
+    /// </summary>
     public class MeshComponent : Component
     {
-        private MeshBase _mesh { get; }
-        public Material Material { get; }
-        public Transform Transform { get; }
+        /// <summary>
+        /// The internal mesh to use for the component.
+        /// </summary>
+        public MeshBase Mesh { get; }
 
-        public MeshComponent(MeshBase mesh, Material material)
+        /// <summary>
+        /// The material to use for rendering the mesh.
+        /// </summary>
+        public Material Material { get; }
+
+        /// <summary>
+        /// The transform that determines the world position of the mesh.<br/>
+        /// This is the transform of the parent entity.
+        /// </summary>
+        public Transform? Transform { get; private set; }
+
+        /// <summary>
+        /// Creates a new mesh component. 
+        /// </summary>
+        /// <param name="mesh">The internal mesh to use for the component.</param>
+        /// <param name="material">The material to use for rendering the mesh.</param>
+        public MeshComponent(MeshBase mesh, Material material) : base(typeof(MeshComponent))
         {
-            _mesh = mesh;
+            Mesh = mesh;
             Material = material;
-            Transform = new Transform();
         }
 
-        public void Render() => _mesh.Render();
 
-        public static implicit operator MeshBase(MeshComponent m) => m._mesh;
+        /// <summary>
+        /// Renders the mesh.
+        /// </summary>
+        public void Render() => Mesh.Render();
+
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public override void Attached()
+        {
+            Transform = Entity?.GetComponent<Transform>();
+
+            if (Transform == null)
+            {
+                Logger.Log(Severity.Fatal, "A mesh can only be attached to components that have a Transform component attached already!");
+            }
+        }
+
+
+        /// <summary>
+        /// Implicit cast to <see cref="MeshBase"/>.
+        /// </summary>
+        public static implicit operator MeshBase(MeshComponent m) => m.Mesh;
     }
 }
