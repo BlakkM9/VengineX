07af735ebc69888aae1f6f40cc0a8febeb92fb60
BlakkM9 <julian.desloges@gmail.com>
2022-04-01 18:19:48 +0200

    Added resize

--- VengineX/UI/CanvasComponent.cs
+++ VengineX/UI/CanvasComponent.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using VengineX.ECS;
 using VengineX.Graphics.Cameras;
 using VengineX.Graphics.Renderers;
+using VengineX.Input;
 
 namespace VengineX.UI
 {
@@ -14,13 +15,23 @@ namespace VengineX.UI
     {
         private readonly Canvas _canvas;
 
+
         public CanvasComponent(Canvas canvas) : base(typeof(CanvasComponent)) => _canvas = canvas;
+
+
+        public CanvasComponent(int width, int height, InputManager input) : base(typeof(CanvasComponent))
+        {
+            _canvas = new Canvas(width, height, input);
+        }
+
 
         public void UpdateEvents() => _canvas.UpdateEvents();
 
         public void UpdateLayout() => _canvas.UpdateLayout();
 
         public Camera Camera => _canvas.Camera;
+
+        public void Resize(int width, int height) => _canvas.Resize(width, height);
 
         public IEnumerable<QuadVertex> EnumerateQuads() => _canvas.EnumerateQuads();
 
